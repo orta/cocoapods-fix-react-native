886f6c13a2a0ead0aa88504a1c696d4c8745a14f
Richard Harrah <topplethenunnery@gmail.com>
2019-03-11 17:35:36 -0400

    add pre-patch to add DoubleConversion and glog to cxxreact

--- lib/cocoapods-fix-react-native/versions/0_58_6-pre.rb
+++ lib/cocoapods-fix-react-native/versions/0_58_6-pre.rb
@@ -1,1 +1,31 @@
 require_relative './0_58_5-pre'
+
+# https://github.com/facebook/react-native/issues/23390
+react_podspec_file = 'React.podspec'
+react_podspec_old_code = '
+  s.subspec "cxxreact" do |ss|
+    ss.dependency             "React/jsinspector"
+    ss.dependency             "boost-for-react-native", "1.63.0"
+    ss.dependency             "Folly", folly_version
+    ss.compiler_flags       = folly_compiler_flags
+    ss.source_files         = "ReactCommon/cxxreact/*.{cpp,h}"
+    ss.exclude_files        = "ReactCommon/cxxreact/SampleCxxModule.*"
+    ss.private_header_files = "ReactCommon/cxxreact/*.h"
+    ss.pod_target_xcconfig  = { "HEADER_SEARCH_PATHS" => "\"$(PODS_TARGET_SRCROOT)/ReactCommon\" \"$(PODS_ROOT)/boost-for-react-native\" \"$(PODS_ROOT)/DoubleConversion\" \"$(PODS_ROOT)/Folly\"" }
+  end
+'
+react_podspec_new_code = '
+  s.subspec "cxxreact" do |ss|
+    ss.dependency             "React/jsinspector"
+    ss.dependency             "boost-for-react-native", "1.63.0"
+    ss.dependency             "Folly", folly_version
+    ss.dependency             "DoubleConversion", "1.1.6"
+    ss.dependency             "glog", "0.3.5"
+    ss.compiler_flags       = folly_compiler_flags
+    ss.source_files         = "ReactCommon/cxxreact/*.{cpp,h}"
+    ss.exclude_files        = "ReactCommon/cxxreact/SampleCxxModule.*"
+    ss.private_header_files = "ReactCommon/cxxreact/*.h"
+    ss.pod_target_xcconfig  = { "HEADER_SEARCH_PATHS" => "\"$(PODS_TARGET_SRCROOT)/ReactCommon\" \"$(PODS_ROOT)/boost-for-react-native\" \"$(PODS_ROOT)/DoubleConversion\" \"$(PODS_ROOT)/Folly\"" }
+  end
+'
+patch_pod_file react_podspec_file, react_podspec_old_code, react_podspec_new_code
